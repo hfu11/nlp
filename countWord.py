993e3090b1f75f7e23090f7c6c15830909edc89b
hfu11 <hfu11@binghamton.edu>
2021-05-26 16:37:20 +0800

    countWordFinished

--- countWord.py
+++ countWord.py
@@ -1,16 +1,21 @@
 import nltk
-import matplotlib.pyplot as plt
-from nltk.probability import FreqDist
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-probability_adverbs = ["probably","possibly","perhaps","maybe","likely","certainly","surely",]
+f=open("target_words.txt","r")
+s=f.read()
+f.close()
+
+target_adverbs = nltk.word_tokenize(s)
+# print(target_adverbs)
 
 filepath = sys.argv[1]
 
 f=open(filepath,"r")
 s=f.read()
 f.close()
-# print(s)
 
 #分词
 text=nltk.word_tokenize(s)
@@ -18,19 +23,28 @@ text=nltk.word_tokenize(s)
 #标记并提取出副词
 tags=nltk.pos_tag(text)
 adverbs = [key for key,value in tags if value == "RB"]
-adverbs2 = [word for word in adverbs if word in probability_adverbs]
-print(adverbs2)
-
-# 去重
-# adverbs=list(set(adverbs))
-# print(adverbs)
-
-plt.ion()
 freq = nltk.FreqDist(adverbs)
-# freq_dict = {}
-# for item in freq:
-#     freq_dict[item] = freq[item]
-# print(freq_dict)
-freq.plot(100)
-plt.savefig('test.png')
-plt.ioff()
+df_freq = pd.DataFrame(freq.items(), columns = ["Word","Frequency"])
+df_freq = df_freq.sort_values(by="Frequency",ascending=False)
+df_freq.to_csv("frequencies_adverbs.csv",index=0)
+
+#目标词
+target = [word for word in text if word in target_adverbs]
+# print(target)
+if len(target):
+    freq = nltk.FreqDist(target)
+    # print(len(target))
+    df_freq = pd.DataFrame(freq.items(), columns = ["Word","Frequency"])
+    df_freq = df_freq.sort_values(by="Frequency",ascending=False)
+    df_freq.to_csv("frequencies_target.csv",index=0)
+    # print(df_freq)
+
+# plt.ion()
+# freq = nltk.FreqDist(target)
+# # freq_dict = {}
+# # for item in freq:
+# #     freq_dict[item] = freq[item]
+# # print(freq_dict)
+# freq.tabulate(100)
+# plt.savefig('test.png')
+# plt.ioff()
